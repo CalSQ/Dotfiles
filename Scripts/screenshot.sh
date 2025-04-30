312d13c4f2cf4b2d973c000b4da9dca53b26cd7b
CalSQ <caffrey.squad@gmail.com>
2025-05-01 00:18:16 +0100

    refactor: changed screenshot utility

--- Scripts/screenshot.sh
+++ Scripts/screenshot.sh
@@ -7,5 +7,6 @@ mkdir -p "$save_dir"
 filename="sc_$(date +'%d-%m-%y_%H-%M-%S-%3N').png"
 filepath="$save_dir/$filename"
 
-grimblast copysave area "$filepath"
+#grimblast copysave area "$filepath"
+grim -g "$(slurp)" "$filepath" && wl-copy < "$filepath"
 #notify-send "Screenshot saved to '$filepath'"
